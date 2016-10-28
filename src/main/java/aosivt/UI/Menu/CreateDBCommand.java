7c93cc498b64a728b71874adc16e9712f2527c75
aosivt <ghost.alex86@gmail.com>
2016-10-28 16:56:44 +0700

    update grid

--- src/main/java/aosivt/UI/Menu/CreateDBCommand.java
+++ src/main/java/aosivt/UI/Menu/CreateDBCommand.java
@@ -1,7 +1,12 @@
 package aosivt.UI.Menu;
 
+import aosivt.Entity.ViewProtocol;
+import aosivt.util.HibernateUtil;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.Notification;
+import org.hibernate.FlushMode;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  * Created by oshchepkovayu on 28.10.16.
@@ -16,5 +21,37 @@ public class CreateDBCommand implements  MenuBar.Command {
     public void menuSelected(MenuBar.MenuItem menuItem) {
         Notification.show("Action " + menuItem.getText(),
                 Notification.Type.TRAY_NOTIFICATION);
+        this.createDB();
+    }
+
+    private void createDB()
+    {
+        ViewProtocol viewProtocol_resolution = new ViewProtocol();
+        ViewProtocol viewProtocol_damage = new ViewProtocol();
+        ViewProtocol viewProtocol_more = new ViewProtocol();
+
+        viewProtocol_resolution.setView_protocol_id(Long.parseLong("1"));
+        viewProtocol_resolution.setView_protocol("Постановление");
+        viewProtocol_damage.setView_protocol_id(Long.parseLong("2"));
+        viewProtocol_damage.setView_protocol("Ущерб");
+        viewProtocol_more.setView_protocol_id(Long.parseLong("3"));
+        viewProtocol_more.setView_protocol("Другое");
+
+
+        insertIntoDBProtocolView(viewProtocol_resolution);
+        insertIntoDBProtocolView(viewProtocol_damage);
+        insertIntoDBProtocolView(viewProtocol_more);
+
+    }
+    private void insertIntoDBProtocolView(ViewProtocol _new_protocol_view)
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.setHibernateFlushMode(FlushMode.MANUAL);
+        Transaction transaction = session.beginTransaction();
+        session.saveOrUpdate(_new_protocol_view);
+        session.flush();
+        transaction.commit();
+
+        session.close();
     }
 }
